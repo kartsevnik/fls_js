7cf1cf69903ce9fa0c756c86fdfac298d13eefca
Nikolaus <mkdiya20@gmail.com>
2023-03-04 22:08:37 +0100

    added new lesson8

---
+++ js/lesson8/task8-1.js
@@ -0,0 +1,13 @@
+// Enter the required data
+
+
+
+// Creating Functions
+
+
+
+//Determining the result
+
+
+// Result output
+// document.write(`<div div class= "js-output"> Average value of positive temperatures = ${result}</div>`)
