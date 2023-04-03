903c7e480e5a69c9f8a55874d1b445131d1de442
Nikolaus <mkdiya20@gmail.com>
2023-04-03 10:24:54 +0200

    fix 15-0

--- js/lesson15/task15-0.js
+++ js/lesson15/task15-0.js
@@ -10,7 +10,7 @@
 function getRandomNumber(minV, maxV) {
     return minV + Math.floor(Math.random() * (maxV - minV + 1))
 }
-function getSizeOfArray(size, minV, maxV) {
+function getArrayWithNumbers(size, minV, maxV) {
     let sizeOfArray = []
     for (let i = 0; i < size; i++) {
         const randomNum = getRandomNumber(minV, maxV)
@@ -20,50 +20,33 @@ function getSizeOfArray(size, minV, maxV) {
 }
 
 let obj1 = {
-    arrayOfValue: getSizeOfArray(10, 0, 100),
+    arrayOfValue: getArrayWithNumbers(10, 0, 10),
 
+    // =========> method:
     getSum: function () {
         return this.arrayOfValue.reduce((preSum, element) => preSum + element, 0)
     }
 }
 
 let obj2 = {
-    arrayOfValue: getSizeOfArray(10, 0, 100),
+    arrayOfValue: getArrayWithNumbers(10, 0, 10),
 
+    // =========> method:
     getMultiplicationInRange: function (minValue, maxValue) {
-        let result = this.arrayOfValue.find((element) =>
-            element > minValue &&
-            element < maxValue &&
-            element !== 0)
-        let resultIndex = (this.arrayOfValue.findIndex((element) =>
-            element > minValue &&
-            element < maxValue &&
-            element !== 0)) + 1
-
-        for (let i = resultIndex; i < this.arrayOfValue.length; i++) {
-            if (this.arrayOfValue[i] !== 0 && this.arrayOfValue[i] > minValue && this.arrayOfValue[i] < maxValue) {
-                result = this.arrayOfValue[i] * result
-            }
-        }
-        return result
+        return this.arrayOfValue.reduce((preResult, num) =>
+            num >= minValue && num <= maxValue ? preResult * num : preResult, 1)
     }
 }
+// ====================================================================================================================== //
 
-console.log(obj1.arrayOfValue);
-let sumOfObj1 = obj1.getSum()
-console.log(`Sum of 1 object: ${sumOfObj1}`);
+console.log(`Array of 1 object: ${obj1.arrayOfValue}`);
+console.log(`Array of 2 object: ${obj2.arrayOfValue}`);
 
-console.log(obj2.arrayOfValue);
-let sumOfObj2 = obj1.getSum.call(obj2)
-console.log(`Sum of 2 object: ${sumOfObj2}`);
+console.log(`Sum of 1 object: ${obj1.getSum()}`);
+console.log(`Sum of 2 object: ${obj1.getSum.call(obj2)}`);
 
-
-let multiplyOfObj1 = obj2.getMultiplicationInRange.apply(obj1, [10, 50])
-console.log(`Multiplication in range obj1: ${multiplyOfObj1}`);
-
-
-let multiplyOfObj2 = obj2.getMultiplicationInRange(10, 50)
-console.log(`Multiplication in range obj2: ${multiplyOfObj2}`);
+console.log(`Multiplication in range of 1 object: ${obj2.getMultiplicationInRange.apply(obj1, [2, 5])}`);
+console.log(`Multiplication in range of 2 object: ${obj2.getMultiplicationInRange(2, 5)}`);
 
 
 
@@ -83,7 +66,7 @@ console.log(`Multiplication in range obj2: ${multiplyOfObj2}`);
 //     // =========> properties:
 //     this.minV = minV
 //     this.maxV = maxV
-//     this.array = this.getSizeOfArray(size)
+//     this.array = this.getArrayWithNumbers(size)
 // }
 
 // // =========> methods:
@@ -91,7 +74,7 @@ console.log(`Multiplication in range obj2: ${multiplyOfObj2}`);
 //     return minV + Math.floor(Math.random() * (maxV - minV + 1))
 // }
 
-// FirstObject.prototype.getSizeOfArray = function (size) {
+// FirstObject.prototype.getArrayWithNumbers = function (size) {
 //     let sizeOfArray = []
 //     for (let i = 0; i < size; i++) {
 //         const randomNum = this.getRandomNumber(this.minV, this.maxV)
@@ -105,13 +88,13 @@ console.log(`Multiplication in range obj2: ${multiplyOfObj2}`);
 //         // =========> properties:
 //         this.minV = minV
 //         this.maxV = maxV
-//         this.array = this.getSizeOfArray(size)
+//         this.array = this.getArrayWithNumbers(size)
 //     }
 //     // =========> methods:
 //     getRandomNumber(minV, maxV) {
 //         return minV + Math.floor(Math.random() * (maxV - minV + 1))
 //     }
-//     getSizeOfArray(size) {
+//     getArrayWithNumbers(size) {
 //         let sizeOfArray = []
 //         for (let i = 0; i < size; i++) {
 //             const randomNum = this.getRandomNumber(this.minV, this.maxV)
