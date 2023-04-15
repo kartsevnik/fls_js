12426813379ab52ae95f59372621a17140d58c00
Nikolaus <mkdiya20@gmail.com>
2023-04-15 19:55:04 +0200

    fix task 17-3

--- js/lesson17/task17-3.js
+++ js/lesson17/task17-3.js
@@ -1,42 +1,55 @@
 // ====================================================================================================================== //
-// Створити клас Нагадувач. 
-// Кожні вказані кількості секунд(використати setInterval) 
-// програма нагадує про якусь подію(це просто текст) 
-// і також виводиться порядковий номер скільки раз вже нагадування було.
-// Зробити так, щоб неможна було зробити одночасно декілька об’єктів - нагадувачів.
-// Методи зупинки таймера, 
-// метод зміни повідомлення без зупинки таймера.
+// Створити клас Нагадувач.
+// + Кожні вказані кількості секунд(використати setInterval)
+// + програма нагадує про якусь подію(це просто текст)
+// + і також виводиться порядковий номер скільки раз вже нагадування було.
+// + Зробити так, щоб неможна було зробити одночасно декілька об’єктів - нагадувачів.
+// + Методи зупинки таймера,
+// + метод зміни повідомлення без зупинки таймера.
 // ====================================================================================================================== //
 class Reminder {
     static verification
     static counter = 0
+    static text
+    static interval
+    static intervalID
+    // =========>
     constructor(text, interval) {
         if (Reminder.verification)
             return Reminder.verification
 
-        this.text = text
-        this.interval = interval
-
+        Reminder.text = text
+        Reminder.interval = interval
         Reminder.verification = this
     }
+    // =========> method:
+    static initReminder() {
 
-    initReminder() {
-        let intervalID = setInterval(() => Reminder.counter++ & console.log(`${this.text} and ${Reminder.counter}`), this.interval * 1000)
+        if (Reminder.initReminder.intervalID) {
+            return Reminder.initReminder.intervalID
+        }
 
+        Reminder.intervalID = setInterval(() => Reminder.counter++ & console.log(`${Reminder.text} and ${Reminder.counter}`), Reminder.interval * 1000)
+        Reminder.initReminder.intervalID = this
+    }
+
+    // =========> method (метод зміни повідомлення без зупинки таймера):
+    static changeText(newText, timeForStart) {
+        setTimeout(() => Reminder.text = newText, timeForStart * 1000)
+    }
+
+    // =========> method (метод зупинки таймера):
+    static stopReminder() {
+        clearInterval(Reminder.intervalID)
     }
 }
+
+
 // ====================================================================================================================== //
-let result = new Reminder(`text text text`, 3)
-let r1 = result.initReminder()
-console.log(Reminder.counter);
-
-let test = new Reminder(`111 222 333`, 2)
-let r2 = test.initReminder()
-let r3 = test.initReminder()
-
-
-
-
+let result = new Reminder(`old text`, 3)
+let initReminder = Reminder.initReminder()
+let changeText = Reminder.changeText(`new text`, 3)
+let stopReminder = setTimeout(() => Reminder.stopReminder(), 10000);
 
 // ====================================================================================================================== //
 
