6fd09ebd7f9d1964b173406fd05c5f5df52d516b
Mykola Kartsev <mykola.kartsev@itsrizzoli.it>
2023-11-29 10:34:13 +0100

    add text task 21-4

---
+++ js/lesson21/task21-4.js
@@ -0,0 +1,9 @@
+// Given 3 dimension tables 3 * 3 with random numbers.
+// If there is a click on any of the cells, then a red frame is added to the appropriate table
+// (Try to add the ability to display the number of clicks near the table name
+// Using the attribute added for this attribute).
+
+// Дано 3 таблиці розмірності 3 * 3 з випадковими числами.
+// Якщо відбувається клік на якійсь із клітинок, то до відповідної таблиці додається червона рамка
+// (спробуйте додати можливість відображення кількості кліків біля назви таблиці
+// з використанням відповідно доданого для цього атрибута).
