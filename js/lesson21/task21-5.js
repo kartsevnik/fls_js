06f9467de67908f0ce591c2046613f8d1c50c628
Mykola Kartsev <mykola.kartsev@itsrizzoli.it>
2023-12-05 16:36:23 +0100

    add html code of task 21-5

---
+++ js/lesson21/task21-5.js
@@ -0,0 +1,9 @@
+// Problem 5.
+// 7 numerical values (age, height, weight, wages, experience, department number, serial number) are introduced on the form.
+
+// Add CLICK event processing event to the form and if the click is on input, then automatically replace the value of it by 0.
+
+// Задача 5.
+// На формі вводять 7 числових значень(вік, зріст, вага, заробітна плата, стаж, номер відділу, порядковий номер).
+
+// Додати подію обробки події click на форму і якщо клік на input, то автоматично замінювати значення його на 0.
