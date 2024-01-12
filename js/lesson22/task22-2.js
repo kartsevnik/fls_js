c51b0a6873d396f97e8ef8494f1d492e9e176548
kartsevnik <kartsevnik@gmail.com>
2024-01-12 16:53:59 +0100

    add resolved task 22-2 and 22-3

--- js/lesson22/task22-2.js
+++ js/lesson22/task22-2.js
@@ -1,17 +1,108 @@
 // ====================================================================================================================== //
-// Розробити Клас House
+// Розробити Класи House, Dog, Bird
 
 // --- властивості-- -
-//     Координата Х
+// Координата Х
 // Координата У
 // шлях до зображення
 // інтервал оновлення
 
 // --- методи-- -
 //     генерування елемента розмітки
-// оновлення через вказаний інтервал(збільшення або зменшення масштабу(об’єкт не рухається)
 
+// House
+// оновлення через вказаний інтервал (збільшення або зменшення масштабу(об’єкт не рухається)
+
+// Dog
+// оновлення через вказаний інтервал (випадкове зміщення по горизонталі (зміна координати Х))
+
+// Bird
+// оновлення через вказаний інтервал (випадкове переміщення у довільному напрямку)
 // ====================================================================================================================== //
+const listOfObjects = [
+    { x: 200, y: 0, url: '../../img/task22-2/house.png', interval: 1000 },
+    { x: 100, y: 50, url: '../../img/task22-2/dog.png', interval: 1000 },
+    { x: 350, y: 50, url: '../../img/task22-2/bird.png', interval: 500 },
+];
+
+class Object {
+    constructor({ x, y, url }) {
+        this.x = x;
+        this.y = y;
+        this.url = url;
+    }
+
+    getRandomValue(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+    renderImage() {
+        const img = document.createElement('img');
+        img.src = this.url;
+        img.setAttribute('width', '10%');
+        img.style.position = 'relative';
+        img.style.top = `${this.y}px`;
+        img.style.left = `${this.x}px`;
+
+        return img;
+    }
+
+    render(selector) {
+        const selectorContent = document.querySelector(selector);
+
+        this.mainContainer = document.createElement('div');
+        this.mainContainer.classList.add('object_task22-2');
+
+        const imgDiv = document.createElement('div');
+        imgDiv.classList.add('object__image_task22-2');
+        imgDiv.appendChild(this.renderImage());
+
+        this.mainContainer.appendChild(imgDiv);
+        selectorContent.appendChild(this.mainContainer);
+    }
+}
+
+class Animation extends Object {
+    constructor({ x, y, url, interval }) {
+        super({ x, y, url });
+        this.interval = interval;
+    }
+
+    initScale(selector) {
+        this.render(selector);
+
+        setInterval(() => {
+            this.mainContainer.style.transform = 'scale(1.1)';
+        }, this.interval);
+
+        setInterval(() => {
+            this.mainContainer.style.transform = 'scale(1)';
+        }, this.interval + 1000);
+    }
+
+    initMoveInHorizontal(selector) {
+        this.render(selector);
+        const img = this.mainContainer.querySelector('img');
+
+        setInterval(() => {
+            img.style.left = `${this.x + this.getRandomValue(-15, 15)}px`;
+        }, this.interval);
+    }
+
+    initRandomMove(selector) {
+        this.render(selector);
+        const img = this.mainContainer.querySelector('img');
+
+        setInterval(() => {
+            img.style.top = `${this.y + this.getRandomValue(-15, 15)}px`;
+            img.style.left = `${this.x + this.getRandomValue(-15, 15)}px`;
+        }, this.interval);
+    }
+}
+
+const house = new Animation(listOfObjects[0]).initScale('.resolve22');
+const dog = new Animation(listOfObjects[1]).initMoveInHorizontal('.resolve22');
+const bird = new Animation(listOfObjects[2]).initRandomMove('.resolve22');
 
 
 
--- js/lesson22/task22-3.js
+++ js/lesson22/task22-3.js
@@ -1,20 +1,107 @@
 // ====================================================================================================================== //
-// Розробити Клас DOG
-
-// --- властивості ---
-// Координата Х
-// Координата У
-// шлях до зображення
-// інтервал оновлення
-
-// --- методи ---
-// генерування елемента розмітки
-// оновлення через вказаний інтервал (випадкове зміщення по горизонталі (зміна координати Х))
+// Задача.Користувач задає
+// місяць навчання учня(перевіряти чи є числом, чи від 1 до 12, чи не канікули) та
+// оцінку(перевіряти чи є числом, чи від 1 до 100).
+// Вивести чи зможе він виправити оцінку(якщо оцінка погана і це не останній місяць у семестрі).
+// Обробку усіх помилок зробити з використанням відповідних класів.
 
 // ====================================================================================================================== //
 
+class verifyEvaluation {
+    constructor() {
+        this.minMonth = 1
+        this.maxMonth = 12
+        this.holiday1 = 6
+        this.holiday2 = 7
+        this.holiday3 = 8
+
+        this.minMark = 1
+        this.maxMark = 100
+        this.message = document.createElement('p')
+    }
+
+    verifyMark(value) {
+        if (isNaN(value) || value < 1 || value > 100) {
+            this.message.innerText = "Недійсна оцінка."
+            this.container.appendChild(this.message);
+            return false
+        } return true
+    }
+
+    verifyMonth(value) {
+        if (isNaN(value) || value < 1 || value > 12) {
+            this.message.innerText = "Недійсний місяць навчання."
+            this.container.appendChild(this.message);
+            return false
+        } else if (value == this.holiday1 || value == this.holiday2 || value == this.holiday3) {
+            this.message.innerText = "Вы на каникулах"
+            this.container.appendChild(this.message);
+            return false
+        } else if (value == 12) {
+            this.message.innerText = "К сожалению вы не сможете исправить оценку"
+            this.container.appendChild(this.message);
+            return false
+        }
+        else if (value >= this.minMonth && value <= (this.maxMonth - 1))
+            this.message.innerText = "Еще есть шанс исправить оценку"
+        this.container.appendChild(this.message);
+        return true
+    }
 
 
+
+    verifyResult() {
+        this.message.innerText = ""
+        const month = this.inputMonth.value
+        const mark = this.inputMark.value
+        let resultMark = this.verifyMark(mark)
+
+        if (resultMark) {
+            this.verifyMonth(month)
+        }
+
+    }
+
+
+
+
+    createContainer(containerID) {
+        const selectorContent = document.querySelector(containerID);
+        this.container = document.createElement('div')
+        this.container.classList.add('task22-2');
+        // =======================================================
+        const labelMonth = document.createElement('label');
+        labelMonth.textContent = "Місяць навчання (1-12): ";
+
+        this.inputMonth = document.createElement('input')
+        this.inputMonth.type = 'number';
+
+        labelMonth.appendChild(this.inputMonth);
+        this.container.appendChild(labelMonth);
+        // =======================================================
+        const labelMark = document.createElement('label');
+        labelMark.textContent = "Оцінка (1-100): ";
+
+        this.inputMark = document.createElement('input')
+        this.inputMark.type = 'number';
+
+        labelMark.appendChild(this.inputMark);
+        this.container.appendChild(labelMark);
+        // =======================================================
+        const btn = document.createElement('button')
+        btn.textContent = "Verify";
+        btn.onclick = this.verifyResult.bind(this)
+        this.container.append(btn)
+        selectorContent.appendChild(this.container);
+    }
+
+    render(containerID) {
+        this.createContainer(containerID)
+    }
+}
+
+let init = new verifyEvaluation();
+init.render('.resolve22')
 // ====================================================================================================================== //
 
 
