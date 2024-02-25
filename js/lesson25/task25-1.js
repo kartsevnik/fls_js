15a6c1444b3a34ab664f376d564ccdf88095313d
mykola-pc-win <kartsevnik@gmail.com>
2024-02-25 23:03:52 +0100

    add resolved task 25-1

--- js/lesson25/task25-1.js
+++ js/lesson25/task25-1.js
@@ -1,3 +1,35 @@
 // Задача 1. Дано клас PhoneNumber.
 // Створити функцію перетворення до string,
-// при якому на основі номера виводиться оператор(050….   MTC, 096…  Kyivstar, ….)
+// при якому на основі номера виводиться оператор(050…. to MTC, 096… to Kyivstar, ….)
+
+class PhoneNumber {
+    constructor(number) {
+        this.number = number;
+    }
+
+    [Symbol.toPrimitive](hint) {
+        if (hint === "string") {
+            return this.getOperator();
+        }
+        return this.number;
+    }
+
+
+    getOperator() {
+        let firstThreeChars = String(this.number);
+        firstThreeChars = firstThreeChars.substring(0, 3);
+        switch (firstThreeChars) {
+            case "050": return "MTC";
+            case "096":
+            case "097": return "Kyivstar";
+            case "093":
+            case "063": return "Life";
+            default: return "Unknown";
+        }
+    }
+
+}
+let phoneString = new PhoneNumber("0506531256")
+console.log(+phoneString);
+let str = "Hello! Your operator is " + String(phoneString)
+console.log(str);
